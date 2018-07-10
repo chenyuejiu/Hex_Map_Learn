57ac22a72bbcd908f74a5b86eadf873fe40c9226
朱龙飞 <398670608@qq.com>
2018-07-10 15:37:47 +0800

    结束分支

---
+++ Assets/Scripts/UI/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿/** 
+ *Copyright(C) 2018 by #COMPANY# 
+ *All rights reserved. 
+ *FileName:     #SCRIPTFULLNAME# 
+ *Author:       #AUTHOR# 
+ *Version:      #VERSION# 
+ *UnityVersion：#UNITYVERSION# 
+ *Date:         #DATE# 
+ *Description:	
+ *History:		
+*/ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
